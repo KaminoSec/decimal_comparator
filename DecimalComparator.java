4495f9c12c5378f2c5e97b2e939984b8d14153f0
ozark-dev <jason@ozarkdev.com>
2022-10-27 23:12:54 -0500

    Initial commit

---
+++ DecimalComparator.java
@@ -0,0 +1,7 @@
+public class DecimalComparator{
+    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2){
+        num1 = (num1*1000);
+        num2 = (num2*1000);
+        return (int)num1 == (int)num2;
+    }
+}
